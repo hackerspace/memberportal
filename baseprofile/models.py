3c271c4e9392595f18494c70448a47ec3ebefbec
Richard Marko <rissko@gmail.com>
2012-04-15 18:42:35 +0200

    Add baseprofile app

---
+++ baseprofile/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
+
+class BaseProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    alt_nick = models.CharField(
+        max_length = 100,
+        blank = True,
+        verbose_name = _('Alternative nick'))
+
+    phone = models.CharField(
+        max_length = 20,
+        blank = True,
+        verbose_name = _('Phone number'))
+
+    accepted = models.BooleanField(
+        default = False,
+        verbose_name = _('Accepted as a member'))
+
+    council = models.BooleanField(
+        default = False,
+        verbose_name =  _('Member of the council'))
+
+    revision = models.BooleanField(
+        default = False,
+        verbose_name = _('Member of the revision committee'))
+
+    payments_id = models.PositiveSmallIntegerField(
+        verbose_name = _('ID used for payments'),
+        help_text = _('Variable symbol'))
