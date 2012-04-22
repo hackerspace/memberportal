a5bf648f5d0ad3f2a75320daf9b7d6cf9982bdd7
Richard Marko <rissko@gmail.com>
2012-04-22 15:42:38 +0200

    Add password change/reset templates, update urls, update base template

--- memberportal/urls.py
+++ memberportal/urls.py
@@ -10,13 +10,10 @@ admin.autodiscover()
 
 urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += patterns('',
-    # url(r'^$', 'fu.views.home', name='home'),
-    # url(r'^{{ project_name }}/', include('{{ project_name}}.foo.urls')),
     (r'^accounts/', include('registration.backends.default.urls')),
-    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
-    (r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
-    (r'^accounts/profile/$', login_required(TemplateView.as_view(
-        template_name="profile.html"))),
+    url(r'^accounts/profile/$',
+        login_required(TemplateView.as_view(template_name="profile.html")),
+        name='auth_profile'),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
