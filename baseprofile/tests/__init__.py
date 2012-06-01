28ce5a8bfc93557feb5f9d3572e3bc2b9f464500
Richard Marko <rissko@gmail.com>
2012-06-01 18:52:16 +0200

    Add __all__ to init

--- baseprofile/tests/__init__.py
+++ baseprofile/tests/__init__.py
@@ -1,3 +1,5 @@
 from baseprofile.tests.login import LoginTests
 from baseprofile.tests.register import RegistrationTests
 from baseprofile.tests.reset import ResetTests
+
+__all__ = [LoginTests, RegistrationTests, ResetTests]
