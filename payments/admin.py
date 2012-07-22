d1fe4f9913c6ba917840fd87e6bf9698f649dbf6
Richard Marko <rissko@gmail.com>
2012-07-22 18:49:01 +0200

    Improved admin interface for payments

--- payments/admin.py
+++ payments/admin.py
@@ -3,8 +3,19 @@ from django.contrib import admin
 from models import Payment
 
 class PaymentAdmin(admin.ModelAdmin):
-    list_display = ('date', 'amount', 'payment_type', 
-    'constant_symbol', 'variable_symbol', 'specific_symbol', 
-    'identification', 'message', 'parsed', 'user')
+    list_display = ('date', 'amount', 'payment_type',
+    'variable_symbol', 'identification', 'message',
+    'correction_required', 'parsed', 'user_not_none',)
+
+    list_editable = ('correction_required', 'parsed',)
+    list_filter = ('correction_required',)
+
+    def user_not_none(self, obj):
+        if obj.user is None:
+            return ''
+        return '%s' % obj.user
+    user_not_none.short_description = 'User'
+
+
 
 admin.site.register(Payment, PaymentAdmin)
