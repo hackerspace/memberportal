e4ae3cbed4b07b21dbdddb62c8abd189bec47a5e
Richard Marko <rissko@gmail.com>
2012-09-08 21:38:20 +0200

    Update tests

--- baseprofile/tests/login.py
+++ baseprofile/tests/login.py
@@ -1,39 +1,39 @@
-from common import SeleniumTests
+from common import SeleniumTest
 
-class LoginTests(SeleniumTests):
+class LoginTests(SeleniumTest):
     fixtures = ['unprivileged-user.json']
 
     def test_login_both_empty(self):
-        self.get_url('/accounts/login/')
-        self.d.find_element_by_xpath('//button[@type="submit"]').click()
+        self.get('/accounts/login/')
+        self.driver.find_element_by_xpath('//button[@type="submit"]').click()
         body = self.wait_for_body()
         self.assertIn('This field is required.', body.text)
 
     def test_login_empty_pass(self):
-        self.get_url('/accounts/login/')
-        user = self.d.find_element_by_name("username")
+        self.get('/accounts/login/')
+        user = self.driver.find_element_by_name("username")
         user.send_keys('baseuser')
-        self.d.find_element_by_xpath('//button[@type="submit"]').click()
+        self.driver.find_element_by_xpath('//button[@type="submit"]').click()
         body = self.wait_for_body()
         self.assertIn('This field is required.', body.text)
 
     def test_login_incorrect_pass(self):
-        self.get_url('/accounts/login/')
-        user = self.d.find_element_by_name("username")
+        self.get('/accounts/login/')
+        user = self.driver.find_element_by_name("username")
         user.send_keys('baseuser')
-        passwd = self.d.find_element_by_name("password")
+        passwd = self.driver.find_element_by_name("password")
         passwd.send_keys('wrong')
-        self.d.find_element_by_xpath('//button[@type="submit"]').click()
+        self.driver.find_element_by_xpath('//button[@type="submit"]').click()
         body = self.wait_for_body()
         self.assertIn('Please enter a correct username and password', body.text)
 
     def test_login_correct_pass(self):
-        self.get_url('/accounts/login/')
-        user = self.d.find_element_by_name("username")
+        self.get('/accounts/login/')
+        user = self.driver.find_element_by_name("username")
         user.send_keys('baseuser')
-        passwd = self.d.find_element_by_name("password")
+        passwd = self.driver.find_element_by_name("password")
         passwd.send_keys('secret')
-        self.d.find_element_by_xpath('//button[@type="submit"]').click()
+        self.driver.find_element_by_xpath('//button[@type="submit"]').click()
         body = self.wait_for_body()
         self.assertIn('Your profile', body.text)
         self.assertNotIn('Administration', body.text)
--- baseprofile/tests/register.py
+++ baseprofile/tests/register.py
@@ -2,26 +2,26 @@ import re
 
 from django.core import mail
 
-from common import SeleniumTests
+from common import SeleniumTest
 
-class RegistrationTests(SeleniumTests):
+class RegistrationTests(SeleniumTest):
     def test_register_empty(self):
-        self.get_url('/accounts/register/')
-        self.d.find_element_by_xpath('//button[@type="submit"]').click()
+        self.get('/accounts/register/')
+        self.driver.find_element_by_xpath('//button[@type="submit"]').click()
         body = self.wait_for_body()
         self.assertIn('This field is required.', body.text)
 
     def test_register(self):
-        self.get_url('/accounts/register/')
-        user = self.d.find_element_by_name('username')
-        email = self.d.find_element_by_name('email')
-        p1 = self.d.find_element_by_name('password1')
-        p2 = self.d.find_element_by_name('password2')
+        self.get('/accounts/register/')
+        user = self.driver.find_element_by_name('username')
+        email = self.driver.find_element_by_name('email')
+        p1 = self.driver.find_element_by_name('password1')
+        p2 = self.driver.find_element_by_name('password2')
         user.send_keys('baseuser')
         email.send_keys('user@example.org')
         p1.send_keys('password')
         p2.send_keys('password')
-        self.d.find_element_by_xpath('//button[@type="submit"]').click()
+        self.driver.find_element_by_xpath('//button[@type="submit"]').click()
         body = self.wait_for_body()
         self.assertIn('Check your email', body.text)
         self.assertEqual(len(mail.outbox), 1)
@@ -30,11 +30,11 @@ class RegistrationTests(SeleniumTests):
         self.assertIsNotNone(match)
 
         key = match.groups(1)
-        self.get_url('/accounts/activate/%s/' % key)
+        self.get('/accounts/activate/%s/' % key)
         body = self.wait_for_body()
         self.assertIn('Your account is now activated', body.text)
 
     def test_wrong_key(self):
-        self.get_url('/accounts/activate/abcdefgh/')
+        self.get('/accounts/activate/abcdefgh/')
         body = self.wait_for_body()
         self.assertIn('Activation failed', body.text)
