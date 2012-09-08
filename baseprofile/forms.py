dbad7815dd700bff11764936dbcaac1dce6b5542
Richard Marko <rissko@gmail.com>
2012-09-08 22:43:40 +0200

    Drop requirement for first/last name

--- baseprofile/forms.py
+++ baseprofile/forms.py
@@ -4,8 +4,8 @@ from models import BaseProfile
 
 class ProfileForm(forms.ModelForm):
     email = forms.EmailField(max_length=100)
-    first_name = forms.CharField(max_length=100)
-    last_name = forms.CharField(max_length=100)
+    first_name = forms.CharField(max_length=100, required=False)
+    last_name = forms.CharField(max_length=100, required=False)
     class Meta:
         fields = ('phone', 'xmpp', 'alt_nick', )
         model = BaseProfile
