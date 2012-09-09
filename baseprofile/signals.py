fe9a3ea46d5172675f53de9ba890f137e4e3ef5b
Richard Marko <rissko@gmail.com>
2012-09-09 15:53:30 +0200

    Implement email notification for status changes

--- baseprofile/signals.py
+++ baseprofile/signals.py
@@ -1,5 +1,9 @@
+from django.conf import settings
+from django.core.mail import mail_managers
 from django.contrib.auth.models import User
+from django.db.models.signals import pre_save
 from django.db.models.signals import post_save
+from django.utils.translation import ugettext_lazy as _
 
 from models import BaseProfile
 
@@ -12,5 +16,24 @@ def create_user_profile(sender, instance, created, **kwargs):
             obj.payments_id = next_id + 1
         obj.save()
 
+def send_mail_if_status_changed(sender, instance, **kwargs):
+    try:
+        obj = BaseProfile.objects.get(pk=instance.pk)
+    except BaseProfile.DoesNotExist:
+        # New object
+        subject = _('New user registered')
+        message = _('New membership request: %s, %s') % (instance.user.username,
+            instance.user.email)
+        mail_managers(subject, message)
+    else:
+        if not obj.status == instance.status:
+            subject = _('Membership status change')
+            message = _('Your status have changed from "%s" to "%s"') % (
+                obj.get_status_display(), instance.get_status_display())
+            instance.user.email_user(subject, message)
+
 def enable():
     post_save.connect(create_user_profile, sender=User)
+
+    if getattr(settings, 'MEMBER_STATUS_EMAIL_NOTIFY', False) == True:
+        pre_save.connect(send_mail_if_status_changed, sender=BaseProfile)
